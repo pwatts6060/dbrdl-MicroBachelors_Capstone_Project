5739909be2c4d56e11629945ac6063c3d4a44568
Patrick Watts <patrick.watts@hotmail.co.uk>
2023-01-19 00:49:23 +0400

    Update App.js

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -3,19 +3,17 @@ import React from 'react';
 import HomePage from './homepage.js';
 import ContactUs from './contactus.js';
 
-<script>
-  window.watsonAssistantChatOptions = {
-    integrationID: "51764ae0-d4e7-4502-9104-8cb68ce65ee7", // The ID of this integration.
-    region: "us-south", // The region your integration is hosted in.
-    serviceInstanceID: "bcf151c9-a2b2-4140-bf76-5c0390938445", // The ID of your service instance.
-    onLoad: function(instance) { instance.render(); }
-  };
-  setTimeout(function(){
-    const t=document.createElement('script');
-    t.src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js";
-    document.head.appendChild(t);
-  });
-</script>
+window.watsonAssistantChatOptions = {
+integrationID: "51764ae0-d4e7-4502-9104-8cb68ce65ee7", // The ID of this integration.
+region: "us-south", // The region your integration is hosted in.
+serviceInstanceID: "bcf151c9-a2b2-4140-bf76-5c0390938445", // The ID of your service instance.
+onLoad: function(instance) { instance.render(); }
+};
+setTimeout(function(){
+const t=document.createElement('script');
+t.src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js";
+document.head.appendChild(t);
+});
 
 class App extends React.Component {
 
